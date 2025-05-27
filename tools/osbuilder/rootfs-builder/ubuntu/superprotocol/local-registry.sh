03ba0ecfee530450ccc2188f01abc9b9e1417f71
Vladyslav Marchuk <134324829+marchuk-vlad@users.noreply.github.com>
2025-05-27 16:41:58 +0300

    Automatic CPU type (#36)
    * Add automatic file-based detection of CPU type to vm service
    
    * Add SGX support
    
    * remove sgx
    
    * Add sev snp mode, change secret.sp-pki-challenge.yaml logic
    
    * Fix tee-pki-curl version
    
    * Change CA Cert and URL
    
    * Add cpu type detection in superprotocol-certs.sh
    
    * rename to sev-snp
    
    * Return old cert for untrust
    
    * minor fix for cpu-type
    
    * Fix string comparison for challenge-type in k8s.yaml.tmpl and update CPU_TYPE detection logic in superprotocol-certs.sh
    
    * Temporary disable forced untrusted mode on debug
    
    * fix(superprotocol-certs): Returned SUPER_CERT_INITIALIZER_URL
    
    * feat: use self-signed ssl certificate for registry
    
    * fix: pki-curl v1.4.7
    
    * feat: create registry certificates during build step
    
    * fix: use /etc/super for registry certificates
    
    * fix: more fixes with cert files
    
    * fix: tee-pki-curl 1.4.8
    
    * fix:  get-secrets url for trusted envs
    
    * fix: add single quotes to get-secrets choices
    
    * fix: sev-snp fixes, new version of pki-curl, subroot3
    
    ---------
    
    Co-authored-by: DrunkBatya <drunkbatya.js@gmail.com>
    Co-authored-by: vasilykraev <github@vkraev.ru>

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/local-registry.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/local-registry.sh
@@ -8,4 +8,4 @@ sleep 3
 mkdir -p /opt/hauler/.hauler
 /usr/local/bin/hauler store load --store /opt/hauler/store /etc/super/opt/hauler/*.zst
 nohup /usr/local/bin/hauler store serve fileserver --store /opt/hauler/store --directory /opt/hauler/registry &
-/usr/local/bin/hauler store serve registry --store /opt/hauler/store --directory /opt/hauler/registry --tls-cert=${SUPER_CERT_FILEPATH}.bundle.crt --tls-key=${SUPER_CERT_FILEPATH}.key
+/usr/local/bin/hauler store serve registry --store /opt/hauler/store --directory /opt/hauler/registry --tls-cert=${SUPER_CERT_FILEPATH}.crt --tls-key=${SUPER_CERT_FILEPATH}.key
--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/postbuild.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/postbuild.sh
@@ -42,7 +42,13 @@ run_postbuild() {
 
     set -x
     cp "${script_dir}/cert/superprotocol-ca.crt" "${rootfs_dir}/usr/local/share/ca-certificates/superprotocol-ca.crt"
-    cp "${script_dir}/cert/ca-initializer-linux" "${rootfs_dir}/usr/local/bin/"
+    cp "${script_dir}/cert/registry.superprotocol.local.ca.crt" "${rootfs_dir}/usr/local/share/ca-certificates/registry.superprotocol.local.ca.crt"
+
+    mkdir -p "${rootfs_dir}/etc/super/certs"
+    cp "${script_dir}/cert/registry.superprotocol.local.ca.crt" "${rootfs_dir}/etc/super/certs/registry.superprotocol.local.ca.crt"
+    cp "${script_dir}/cert/registry.superprotocol.local.key" "${rootfs_dir}/etc/super/certs/registry.superprotocol.local.key"
+    cp "${script_dir}/cert/registry.superprotocol.local.crt" "${rootfs_dir}/etc/super/certs/registry.superprotocol.local.crt"
+
     cp "${script_dir}/cert/superprotocol-certs.sh" "${rootfs_dir}/usr/local/bin/"
     cp "${script_dir}/cert/superprotocol-certs.service" "${rootfs_dir}/etc/systemd/system"
     ln -s /etc/systemd/system/superprotocol-certs.service "$rootfs_dir/etc/systemd/system/multi-user.target.wants/superprotocol-certs.service"

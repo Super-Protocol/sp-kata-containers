81426e8aed9fbd89d6b9ddba22f14ef637cf126b
Petr Evstifeev <7293601+Villain88@users.noreply.github.com>
2025-05-28 14:17:45 +0300

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -x
 apt update
 
 cd /opt/deb/nvidia
@@ -8,7 +8,7 @@ dpkg -i *.deb
 apt update
 if ! DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-driver-550-open; then
     echo "Error when install nvidia drivers"
-    cat /var/lib/dkms/nvidia/550.163.01/build/make.log
+    cat /var/lib/dkms/nvidia/550.163.01/build/make.log || echo "make.log not found"
     exit 1
 fi
 cd /opt/deb

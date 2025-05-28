530bbbc8877a12bf7991313b70e7e5f614f2a834
Petr Evstifeev <7293601+Villain88@users.noreply.github.com>
2025-05-28 13:51:17 +0300

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+set -e
 apt update
 
 cd /opt/deb/nvidia
 dpkg -i *.deb
 apt update
-DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-driver-550-open
+if ! DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-driver-550-open; then
+    echo "Error when install nvidia drivers"
+    cat /var/lib/dkms/nvidia/550.163.01/build/make.log
+    exit 1
+fi
 cd /opt/deb
 dpkg -i *.deb

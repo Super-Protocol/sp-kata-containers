d58340762a170529645988179b9a9a25cd602465
Andrey Pogoreltsev <andrey.pogoreltsev@gmail.com>
2025-07-07 14:55:04 +0300

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -5,6 +5,6 @@ apt update
 cd /opt/deb/nvidia
 dpkg -i *.deb
 apt update
-DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-driver-550-open
+DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-driver-570-open nvidia-fabricmanager-570
 cd /opt/deb
 dpkg -i *.deb

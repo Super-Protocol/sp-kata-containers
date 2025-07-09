b6339c8c27af2f11283b258e889ef87afff5ab06
Andrey Pogoreltsev <andrey.pogoreltsev@gmail.com>
2025-07-09 15:18:20 +0300

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -7,12 +7,22 @@ dpkg -i *.deb
 apt update
 #apt list --all-versions 2>/dev/null | grep nvidia | grep -E "(570|fabricmanager)"
 DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends \
-    nvidia-headless-no-dkms-570-open=570.133.20-0ubuntu1 \
-    nvidia-fabricmanager-570=570.133.20-1 \
-    libnvidia-nscq-570=570.133.20-1 \
-    nvidia-utils-570=570.133.20-0ubuntu1 \
-    libnvidia-compute-570=570.133.20-0ubuntu1 \
-    nvidia-kernel-common-570=570.133.20-0ubuntu1
+    nvidia-driver-570-open=570.148.08-0ubuntu1 \
+    libnvidia-gl-570=570.148.08-0ubuntu1 \
+    nvidia-dkms-570-open=570.148.08-0ubuntu1 \
+    nvidia-kernel-common-570=570.148.08-0ubuntu1 \
+    nvidia-kernel-source-570-open=570.148.08-0ubuntu1 \
+    libnvidia-compute-570=570.148.08-0ubuntu1 \
+    libnvidia-extra-570=570.148.08-0ubuntu1 \
+    nvidia-compute-utils-570=570.148.08-0ubuntu1 \
+    libnvidia-decode-570=570.148.08-0ubuntu1 \
+    libnvidia-encode-570=570.148.08-0ubuntu1 \
+    nvidia-utils-570=570.148.08-0ubuntu1 \
+    xserver-xorg-video-nvidia-570=570.148.08-0ubuntu1 \
+    libnvidia-cfg1-570=570.148.08-0ubuntu1 \
+    libnvidia-fbc1-570=570.148.08-0ubuntu1 \
+    nvidia-fabricmanager-570=570.148.08-1 \
+    libnvidia-nscq-570=570.148.08-1
 systemctl enable nvidia-fabricmanager
 systemctl enable nvidia-persistenced
 cd /opt/deb

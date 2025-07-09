470b1508bcdc5b763d28858093dbb6ba200c6aaf
Andrey Pogoreltsev <andrey.pogoreltsev@gmail.com>
2025-07-09 18:08:54 +0300

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -6,7 +6,7 @@ cd /opt/deb/nvidia
 dpkg -i *.deb
 apt update
 #apt list --all-versions 2>/dev/null | grep nvidia | grep -E "(570|fabricmanager)"
-DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends --allow-downgrades nvidia-driver-570-open=570.86.15-0ubuntu1 nvidia-persistenced-570=570.86.15-1
+DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends --allow-downgrades nvidia-driver-570-open nvidia-fabricmanager-570
 systemctl enable nvidia-persistenced
 cd /opt/deb
 dpkg -i *.deb

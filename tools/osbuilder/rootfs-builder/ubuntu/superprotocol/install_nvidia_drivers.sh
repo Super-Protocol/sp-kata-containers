83e4b6f3c009d45905858a64244bfeb33f78f4c5
Vasily Kraev <github@vkraev.ru>
2025-07-09 10:23:34 +0300

    Update install_nvidia_drivers.sh, pin versions

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -5,6 +5,6 @@ apt update
 cd /opt/deb/nvidia
 dpkg -i *.deb
 apt update
-DEBIAN_FRONTEND=noninteractive apt install -y cuda-toolkit-12-8 nvidia-driver-570-open nvidia-fabricmanager-570
+DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-driver-570-open=570.133.07-0ubuntu0.24.04.1  nvidia-fabricmanager-570=570.133.20-0ubuntu0.24.04.1
 cd /opt/deb
 dpkg -i *.deb

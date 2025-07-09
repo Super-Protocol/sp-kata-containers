50c051f05acbc5c1aab7c817f6248e1ae78be92a
Andrey Pogoreltsev <andrey.pogoreltsev@gmail.com>
2025-07-09 14:32:21 +0300

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -6,6 +6,9 @@ cd /opt/deb/nvidia
 dpkg -i *.deb
 apt update
 apt list --all-versions 2>/dev/null | grep nvidia | grep -E "(570|fabricmanager)"
-DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-driver-570-server-open=570.133.20-0ubuntu0.24.04.1 nvidia-fabricmanager-570=570.133.20-0ubuntu0.24.04.1
+DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends \
+    nvidia-driver-570-open=570.133.20-0ubuntu1 \
+    nvidia-fabricmanager-570=570.133.20-1 \
+    libnvidia-nscq-570=570.133.20-1
 cd /opt/deb
 dpkg -i *.deb

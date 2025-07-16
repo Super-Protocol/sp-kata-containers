549108bb43ba2f99dc27abcf6c2bfc278cb85f26
vasilykraev <github@vkraev.ru>
2025-07-16 13:00:17 +0300

    back to working versions nvidia drivers & gpu-operator

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -5,8 +5,6 @@ apt update
 cd /opt/deb/nvidia
 dpkg -i *.deb
 apt update
-#apt list --all-versions 2>/dev/null | grep nvidia | grep -E "(570|fabricmanager)"
-DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends --allow-downgrades nvidia-driver-570-open nvidia-fabricmanager-570
-systemctl enable nvidia-persistenced
+DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-driver-550-open
 cd /opt/deb
 dpkg -i *.deb

108d298a94ac068d576e377df661bc1de199f8fb
Vasily Kraev <github@vkraev.ru>
2025-07-21 17:06:02 +0300

    typofix in install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -5,7 +5,7 @@ apt update
 cd /opt/deb/nvidia
 dpkg -i *.deb
 apt update
-DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-driver-570-open nvidia-fabricmanager-570 nvlink5-570 libibumad3 infiniband-diag
+DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-driver-570-open nvidia-fabricmanager-570 nvlink5-570 libibumad3 infiniband-diags
 systemctl enable nvidia-fabricmanager
 cd /opt/deb
 dpkg -i *.deb

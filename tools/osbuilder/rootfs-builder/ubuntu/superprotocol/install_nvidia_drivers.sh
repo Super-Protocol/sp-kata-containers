fefefddb4d17c6d8c2b0ef94c4d61d787ba60c69
Andrey Pogoreltsev <andrey.pogoreltsev@gmail.com>
2025-07-22 01:11:43 +0300

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -7,5 +7,6 @@ dpkg -i *.deb
 apt update
 DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-driver-570-open nvidia-fabricmanager-570 nvlink5-570 libibumad3 infiniband-diags
 systemctl enable nvidia-fabricmanager
+systemctl enable nvidia-persistenced
 cd /opt/deb
 dpkg -i *.deb

3fe1ebd145193d773709ef4f3b787f0e488b6153
Andrey Pogoreltsev <andrey.pogoreltsev@gmail.com>
2025-07-22 22:21:33 +0300

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -3,16 +3,18 @@
 
 echo "Installing MLNX_OFED..."
 apt update
-apt install -y wget curl build-essential dkms
-sudo apt install -y linux-headers-$(uname -r) linux-headers-generic
+apt install -y linux-headers-generic || true
+apt install -y build-essential dkms autotools-dev libnl-route-3-200 flex libusb-1.0-0 tk libpci3 bison libltdl-dev autoconf libnl-3-dev libnuma1 ethtool graphviz libnl-route-3-dev debhelper pkg-config libfuse2t64 pciutils swig lsof chrpath m4 libgfortran5 tcl automake quilt gfortran
 
 cd /tmp
 wget -nv https://www.mellanox.com/downloads/ofed/MLNX_OFED-24.10-3.2.5.0/MLNX_OFED_LINUX-24.10-3.2.5.0-ubuntu24.04-x86_64.tgz
 tar -xzf MLNX_OFED_LINUX-24.10-3.2.5.0-ubuntu24.04-x86_64.tgz
 cd MLNX_OFED_LINUX-24.10-3.2.5.0-ubuntu24.04-x86_64
 
-./mlnxofedinstall --hpc --without-fw-update --force --dkms -v
-
+./mlnxofedinstall --hpc --without-fw-update --force \
+  --skip-distro-check --without-depcheck --without-dkms \
+  --skip-unsupported-devices-check -v
+  
 echo "Installing NVIDIA drivers and components..."
 cd /opt/deb/nvidia
 dpkg -i *.deb

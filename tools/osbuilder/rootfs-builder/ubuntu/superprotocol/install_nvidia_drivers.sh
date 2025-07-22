6efb5bb4b796a26f03ae13a2f16f6ae22378dbc6
Andrey Pogoreltsev <andrey.pogoreltsev@gmail.com>
2025-07-22 23:27:27 +0300

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -9,17 +9,14 @@ dpkg -i *.deb
 
 echo "Installing MLNX_OFED..."
 apt update
-apt install -y linux-headers-6.12.13-nvidia-gpu-confidential || true
-apt install -y build-essential wget dkms autotools-dev libnl-route-3-200 flex libusb-1.0-0 tk libpci3 bison libltdl-dev autoconf libnl-3-dev libnuma1 ethtool graphviz libnl-route-3-dev debhelper pkg-config libfuse2t64 pciutils swig lsof chrpath m4 libgfortran5 tcl automake quilt gfortran
 
 cd /tmp
 wget -nv https://www.mellanox.com/downloads/ofed/MLNX_OFED-24.10-3.2.5.0/MLNX_OFED_LINUX-24.10-3.2.5.0-ubuntu24.04-x86_64.tgz
 tar -xzf MLNX_OFED_LINUX-24.10-3.2.5.0-ubuntu24.04-x86_64.tgz
 cd MLNX_OFED_LINUX-24.10-3.2.5.0-ubuntu24.04-x86_64
 
-./mlnxofedinstall --hpc --without-fw-update --force --skip-repo \
-  --add-kernel-support -k 6.12.13-nvidia-gpu-confidential -v
-  
+./mlnxofedinstall --hpc --without-fw-update --force --dkms --add-kernel-support -k 6.12.13-nvidia-gpu-confidential -v
+
 echo "Installing NVIDIA drivers and components..."
 DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends \
     nvidia-open-570 \

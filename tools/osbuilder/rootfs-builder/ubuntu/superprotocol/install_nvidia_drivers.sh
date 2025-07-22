83fe2e6bce58246a16e51cbe4604ae69eb719758
Andrey Pogoreltsev <andrey.pogoreltsev@gmail.com>
2025-07-22 09:54:01 +0300

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -5,7 +5,15 @@ apt update
 cd /opt/deb/nvidia
 dpkg -i *.deb
 apt update
-DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-driver-570-open nvidia-fabricmanager-570 nvlink5-570 libibumad3 infiniband-diags
+echo "Installing UCX..."
+DEBIAN_FRONTEND=noninteractive apt install -y libucx-dev ucx-tools
+echo "Installing NVIDIA drivers and components..."
+DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends \
+    nvidia-open-570 \
+    nvlink5-570 \
+    nvidia-fabricmanager-570 \
+    libibumad3 \
+    infiniband-diags
 systemctl enable nvidia-fabricmanager
 systemctl enable nvidia-persistenced
 cd /opt/deb

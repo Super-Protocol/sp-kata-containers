c3b52d40c5833128b281e69e97ca53d4efd23212
Vasily Kraev <github@vkraev.ru>
2025-07-22 07:11:09 +0300

    Update install_nvidia_drivers.sh, add  libucx-dev libucx0

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -5,7 +5,7 @@ apt update
 cd /opt/deb/nvidia
 dpkg -i *.deb
 apt update
-DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-driver-570-open nvidia-fabricmanager-570 nvlink5-570 libibumad3 infiniband-diags
+DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends libucx-dev libucx0 nvidia-driver-570-open nvidia-fabricmanager-570 nvlink5-570 libibumad3 infiniband-diags
 systemctl enable nvidia-fabricmanager
 systemctl enable nvidia-persistenced
 cd /opt/deb

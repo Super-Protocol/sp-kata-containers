2776cec1e8a2ac9d2e17e8b29c87bea882bbe70c
Andrey Pogoreltsev <andrey.pogoreltsev@gmail.com>
2025-07-23 00:07:53 +0300

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -16,8 +16,9 @@ wget -nv https://www.mellanox.com/downloads/ofed/MLNX_OFED-24.10-3.2.5.0/MLNX_OF
 tar -xzf MLNX_OFED_LINUX-24.10-3.2.5.0-ubuntu24.04-x86_64.tgz
 cd MLNX_OFED_LINUX-24.10-3.2.5.0-ubuntu24.04-x86_64
 
-./mlnxofedinstall --hpc --without-fw-update --force --dkms --add-kernel-support -k 6.12.13-nvidia-gpu-confidential -vvv
-
+./mlnxofedinstall --guest --without-fw-update --force --dkms \
+  --add-kernel-support -k 6.12.13-nvidia-gpu-confidential \
+  --with-nvmf --fwctl -vvv
 
 echo "Installing NVIDIA drivers and components..."
 DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends \

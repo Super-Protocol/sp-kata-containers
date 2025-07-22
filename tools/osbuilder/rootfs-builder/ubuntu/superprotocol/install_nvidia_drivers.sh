db986c7dbef7dedbe11e994b3b4f9baa66983d74
Andrey Pogoreltsev <andrey.pogoreltsev@gmail.com>
2025-07-22 12:44:04 +0300

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-apt update
 
 echo "Installing MLNX_OFED..."
+apt update
 apt install -y wget curl build-essential dkms
 
 cd /tmp
-wget https://www.mellanox.com/downloads/ofed/MLNX_OFED-24.10-3.2.5.0/MLNX_OFED_LINUX-24.10-3.2.5.0-ubuntu24.04-x86_64.tgz
+wget -nv https://www.mellanox.com/downloads/ofed/MLNX_OFED-24.10-3.2.5.0/MLNX_OFED_LINUX-24.10-3.2.5.0-ubuntu24.04-x86_64.tgz
 tar -xzf MLNX_OFED_LINUX-24.10-3.2.5.0-ubuntu24.04-x86_64.tgz
 cd MLNX_OFED_LINUX-24.10-3.2.5.0-ubuntu24.04-x86_64
 ./mlnxofedinstall --hpc --user-space-only --without-fw-update --force -v
 
-apt update
 echo "Installing NVIDIA drivers and components..."
+cd /opt/deb/nvidia
+dpkg -i *.deb
+apt update
 DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends \
     nvidia-open-570 \
     nvlink5-570 \

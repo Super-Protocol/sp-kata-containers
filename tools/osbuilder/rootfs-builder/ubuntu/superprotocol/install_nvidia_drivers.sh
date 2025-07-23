a22bc388671192ebb2edcca549d094fa6ef60d61
Andrey Pogoreltsev <andrey.pogoreltsev@gmail.com>
2025-07-23 12:49:58 +0300

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -6,14 +6,25 @@ dpkg -i *.deb
 cd /opt/deb/nvidia
 dpkg -i *.deb
 
-echo "Installing DOCA_OFED..."
-apt update
-apt install -y wget
+echo "Installing MFT 4.32.0-120 manually..."
+cd /tmp
+
+echo "Removing old MFT version..."
 apt remove mft -y
 apt autoremove -y
-apt clean
-apt update
 
+echo "Downloading MFT 4.32.0-120..."
+wget --no-verbose https://www.mellanox.com/downloads/MFT/mft-4.32.0-120-x86_64-deb.tgz
+
+echo "Extracting MFT package..."
+tar -xzf mft-4.32.0-120-x86_64-deb.tgz
+cd mft-4.32.0-120-x86_64-deb
+
+echo "Installing MFT..."
+./install.sh
+
+echo "Installing DOCA_OFED..."
+cd /tmp
 wget --no-verbose https://www.mellanox.com/downloads/DOCA/DOCA_v3.0.0/host/doca-host_3.0.0-058000-25.04-ubuntu2404_amd64.deb
 dpkg -i doca-host_3.0.0-058000-25.04-ubuntu2404_amd64.deb
 apt-get update

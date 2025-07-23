77b629cbd650026493e43261e95863ef204d5af2
Andrey Pogoreltsev <andrey.pogoreltsev@gmail.com>
2025-07-23 12:39:50 +0300

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -9,6 +9,11 @@ dpkg -i *.deb
 echo "Installing DOCA_OFED..."
 apt update
 apt install -y wget
+apt remove mft -y
+apt autoremove -y
+apt clean
+apt update
+
 wget --no-verbose https://www.mellanox.com/downloads/DOCA/DOCA_v3.0.0/host/doca-host_3.0.0-058000-25.04-ubuntu2404_amd64.deb
 dpkg -i doca-host_3.0.0-058000-25.04-ubuntu2404_amd64.deb
 apt-get update

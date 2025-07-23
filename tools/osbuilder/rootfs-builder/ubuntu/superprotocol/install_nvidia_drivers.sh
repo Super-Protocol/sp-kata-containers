b122a2a630edf7477051eab24e4b02832bb60864
Andrey Pogoreltsev <andrey.pogoreltsev@gmail.com>
2025-07-23 22:30:46 +0300

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -8,12 +8,12 @@ dpkg -i *.deb
 cd /opt/deb/nvidia
 dpkg -i *.deb
 
-wget https://www.mellanox.com/downloads/MFT/mft-4.32.0-120-x86_64-deb.tgz
+wget -nv https://www.mellanox.com/downloads/MFT/mft-4.32.0-120-x86_64-deb.tgz
 tar -xzf mft-4.32.0-120-x86_64-deb.tgz
 cd mft-4.32.0-120-x86_64-deb
 ./install.sh
 
-wget https://www.mellanox.com/downloads/DOCA/DOCA_v2.10.0/host/doca-host_2.10.0-093000-25.01-ubuntu2404_amd64.deb
+wget -nv https://www.mellanox.com/downloads/DOCA/DOCA_v2.10.0/host/doca-host_2.10.0-093000-25.01-ubuntu2404_amd64.deb
 dpkg -i doca-host_2.10.0-093000-25.01-ubuntu2404_amd64.deb
 apt-get update
 apt-get -y install doca-ofed

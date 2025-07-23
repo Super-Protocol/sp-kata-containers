9214f18322eda81386ff707320a69af297b87530
Andrey Pogoreltsev <andrey.pogoreltsev@gmail.com>
2025-07-23 22:52:41 +0300

    Update install_nvidia_drivers.sh

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -9,9 +9,10 @@ cd /opt/deb/nvidia
 dpkg -i *.deb
 
 apt update
-wget -nv https://www.mellanox.com/downloads/MFT/mft-4.32.0-120-x86_64-deb.tgz
-tar -xzf mft-4.32.0-120-x86_64-deb.tgz
-cd mft-4.32.0-120-x86_64-deb
+cd /tmp
+wget -nv https://www.mellanox.com/downloads/MFT/mft-4.31.0-149-x86_64-deb.tgz
+tar -xzf mft-4.31.0-149-x86_64-deb.tgz
+cd mft-4.31.0-149-x86_64-deb
 ./install.sh --without-kernel
 
 wget -nv https://www.mellanox.com/downloads/DOCA/DOCA_v2.10.0/host/doca-host_2.10.0-093000-25.01-ubuntu2404_amd64.deb

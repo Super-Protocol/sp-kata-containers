4dfd5e8ceda3048f0d62cc9b128598301a9e9654
vasilykraev <github@vkraev.ru>
2025-08-01 16:32:46 +0300

    back to nvidia-550 and gpu-operator-24.9.1

--- tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
+++ tools/osbuilder/rootfs-builder/ubuntu/superprotocol/install_nvidia_drivers.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
 apt update
-apt install -y wget apt-utils dkms
-
-cd /opt/deb
-dpkg -i *.deb
 cd /opt/deb/nvidia
 dpkg -i *.deb
-
 apt update
-cd /tmp
 
-echo "Installing NVIDIA drivers and components..."
-DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-open-575
+DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends nvidia-driver-550-open
+cd /opt/deb
+dpkg -i *.deb
